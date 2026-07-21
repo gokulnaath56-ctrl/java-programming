d33b97fa9dead14c97a365a30960b0c2f77f742b
gokulnaath56-ctrl <gokulnaath56@gmail.com>
2026-07-21 02:11:06 -0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,19 @@
+public class StudentDemo {
+   static class student{
+   int roll no;
+   string name;
+student(int r,Stringn){
+  roll no = r;
+  name=r;
+  system.out-println("constructor
+     called for roll no:"+rollno);
+}
+void display(){
+      system.out-println("Roll No:"+rollno+"\+Name:"+name);
+}
+
+
+protected void finalize(){
+   system.out.println("destructor(finalize) called for Rollno:"+rollno);
+}
+)
